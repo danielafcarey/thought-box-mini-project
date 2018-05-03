0ddc401b0732f3f2a96113e8493bb771529d8c53
Daniela Carey <danielafcarey@gmail.com>
2018-05-03 12:31:08 -0600

    Add functionality for thoughtCard Component and pass all tests

--- src/thoughtCard/thoughtCard.test.js
+++ src/thoughtCard/thoughtCard.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import { ThoughtCard } from './thoughtCard.js';
 
 describe('ThoughtCard', () => {
 
   it('should match the snapshot', () => {
+    const mockProps = { title: 'title', body: 'body' }
+    const thoughtCard = renderer.create(
+      <ThoughtCard title={ mockProps.title } 
+                   body={ mockProps.body }
+      />);
+
+    expect(thoughtCard).toMatchSnapshot();
   });
 
 });
--- src/thoughtList/thoughtList.js
+++ src/thoughtList/thoughtList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThoughtCard } from '../thoughtCard/thoughtCard.js';
 
 export const ThoughtList = ({ thoughtList }) => {
+  const thoughts = thoughtList.map((thought, index) => {
+    return <ThoughtCard key={ `thought${index}` }
+                    title={ thought.title }
+                    body={ thought.body }
+           />
+  })
 
   return (
-    <div>
-
+    <div className='thought-list'>
+      { thoughts }
     </div>
   );
 }
